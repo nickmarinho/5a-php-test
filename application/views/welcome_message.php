891e09af695d440e7a52c2f279a26ec8300afcb1
LUCIANO MARINHO <nickmarinho@gmail.com>
2018-05-11 14:38:44 -0300

    Changing the jQuery version to the latest.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -242,8 +242,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/jquery/1.12.4/jquery.min.js"><\/script>')</script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/jquery/3.3.1/jquery.min.js"><\/script>')</script>
     <script src="assets/js/bootstrap.min.js"></script>
     <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
     <script src="assets/js/vendor/holder.min.js"></script>
